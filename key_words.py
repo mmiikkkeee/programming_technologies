ecf83ce2ac278838c48f3ccd15d0d5026316aa64
mmiikkkeee <83709858+mmiikkkeee@users.noreply.github.com>
2023-12-19 00:00:56 +0300

    key_words.py

--- key_words.py
+++ key_words.py
@@ -1,18 +1,18 @@
 import yake
 
-extractor = yake.KeywordExtractor(lan = "en",     # язык
-                                  n = 3,          # максимальное количество слов в фразе
-                                  dedupLim = 0.3, # порог похожести слов
-                                  top = 10)        # количество ключевых слов
+extractor = yake.KeywordExtractor(lan="en",     # Задаем язык (английский)
+                                  n=3,           # Максимальное количество слов в ключевой фразе
+                                  dedupLim=0.3,   # Порог похожести слов
+                                  top=10)         # Количество ключевых слов для извлечения
 
 sentence = 'one Get it from your two employer It!x27s better and cheaper than what you can get in the market. You are ! allowed to supplement  this may or may not make sense but your final coverage will ! be a lot cheaper if you start with employer ? insurance'
 
 def key_words(sentence):
-    key_tuples = extractor.extract_keywords(sentence)
+    key_tuples = extractor.extract_keywords(sentence)  # Извлекаем ключевые фразы из предложения
     key_words = []
 
-    for tuple in key_tuples:
-        key_words.append(tuple[0])
+    for key_tuple in key_tuples:
+        key_words.append(key_tuple[0])
 
     return key_words
 
