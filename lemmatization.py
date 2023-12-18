b5d1ce7a1b4846d3da04ffb2f40d5db1e9e89990
mmiikkkeee <83709858+mmiikkkeee@users.noreply.github.com>
2023-12-19 00:00:27 +0300

    lemmatization.py

--- lemmatization.py
+++ lemmatization.py
@@ -3,9 +3,9 @@ import spacy
 sentence = 'one Get it from your two employer It!x27s better and cheaper than what you can get in the market. You are ! allowed to supplement  this may or may not make sense but your final coverage will ! be a lot cheaper if you start with employer ? insurance'
 
 def lemmatization(sentence):
-    nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner']) # Инициализируем пространственную модель 'en', сохранив только компонент теггера, необходимый для лемматизации
+    nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner']) # Загружаем английскую модель языка Spacy с отключенными компонентами 'parser' и 'ner', так как они не нужны для лемматизации
     doc = nlp(sentence)
-    sentence = " ".join([token.lemma_ for token in doc]) # Лемматизируем по слову и добавляем в предложение
+    sentence = " ".join([token.lemma_ for token in doc]) # Проходим по каждому токену в предложении, извлекаем его лемму и объединяем в строку
     return sentence
 
 print(lemmatization(sentence))
