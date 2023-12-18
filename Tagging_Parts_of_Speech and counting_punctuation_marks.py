67b4f7530097d48304370675c0af156506e72a2f
mmiikkkeee <83709858+mmiikkkeee@users.noreply.github.com>
2023-12-19 00:03:07 +0300

    Tagging_Parts_of_Speech and counting_punctuation_marks.py

--- Tagging_Parts_of_Speech and counting_punctuation_marks.py
+++ Tagging_Parts_of_Speech and counting_punctuation_marks.py
@@ -1,34 +1,46 @@
 import nltk
 from nltk.tokenize import word_tokenize
 
-#nltk.download('punkt')
+#nltk.download('punkt') 
 #nltk.download('averaged_perceptron_tagger')
+
+# Задаем исходное предложение
 sentence = 'one Get it from your two employer It!x27s better and cheaper than what you can get in the market. You are ! allowed to supplement  this may or may not make sense but your final coverage will ! be a lot cheaper if you start with employer ? insurance'
 
 def tagging_words_from_sentence(sentence):
+    # Инициализация счетчиков для различных частей речи
     adjectives_count, adverbs_count, nouns_count, verbs_count, numerals_count = 0, 0, 0, 0, 0
-
+    
+    # Токенизация слов в предложении
     words_in_sentence = word_tokenize(sentence)
+    
+    # Получение частей речи для каждого токена
     tags_of_words = nltk.pos_tag(words_in_sentence)
-
+    
+    # Подсчет количества слов каждой части речи
     for word in tags_of_words:
-        if  word[1] == 'JJ':
+        if word[1] == 'JJ':
             adjectives_count += 1
-        if  word[1] == 'RB':
+        if word[1] == 'RB':
             adverbs_count += 1
-        if  word[1] == 'NN':
+        if word[1] == 'NN':
             nouns_count += 1
-        if  word[1] == 'VB':
+        if word[1] == 'VB':
             verbs_count += 1
-        if  word[1] == 'CD':
+        if word[1] == 'CD':
             numerals_count += 1
-
+    
+    # Возвращаем словарь с результатами подсчета
     return {'adjectives_count': adjectives_count, 'adverbs_count': adverbs_count, 'nouns_count': nouns_count, 'verbs_count': verbs_count, 'numerals_count': numerals_count}
 
 def counting_punctuation_marks(sentence):
+    # Разбиваем предложение на слова
     words = sentence.split()
+    
+    # Инициализация счетчиков знаков препинания
     question_marks_count, exclamation_marks_count = 0, 0
-
+    
+    # Подсчет количества вопросительных и восклицательных знаков
     for word in words:
         for symb in word:
             if symb == '!':
@@ -36,7 +48,9 @@ def counting_punctuation_marks(sentence):
             if symb == '?':
                 exclamation_marks_count += 1
             
+    # Возвращаем словарь с результатами подсчета
     return {'question_marks_count': question_marks_count, 'exclamation_marks_count': exclamation_marks_count}
 
+# Выводим результаты подсчета знаков препинания и частей речи
 print(counting_punctuation_marks(sentence))
 print(tagging_words_from_sentence(sentence))
